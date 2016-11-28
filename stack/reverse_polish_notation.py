e81781853ef3dc9c4ddaeb3937b4189d27e77cbf
Yuancong Deng <yuancong.deng@ele.me>
2016-11-28 20:27:39 +0800

    'reverse polish notation' undo

---
+++ stack/reverse_polish_notation.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+
+
+
+def convert_to_rpn(infix_expression):
+    """
+    将中缀表达式转为后缀表达式(逆波兰表达式)
+    :param infix_expression:
+    :return:
+    """
+    pass
+
+
+def calclulate_rpn(suffix_expression):
+    """
+    计算后缀表达式(逆波兰表达式)的值
+    :param suffix_expression:
+    :return:
+    """
+    pass
+
+
+
+if __name__ == '__main__':
+    infix_expression = ""
+
